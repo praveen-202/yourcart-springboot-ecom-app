7c27df9447296cd167faf67baa7c7699db20ac98
Praveen Kumar <kumarpraveen7604@gmail.com>
2025-10-23 17:21:55 +0530

    created a components/Card.jsx

---
+++ ecommerce/src/components/Card.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = ({ name, studentClass, roll, school }) => {
+  return (
+    <div className="card">
+      <h2>Student Details</h2>
+      <p><strong>Name:</strong> {name}</p>
+      <p><strong>Class:</strong> {studentClass}</p>
+      <p><strong>Roll No:</strong> {roll}</p>
+      <p><strong>School:</strong> {school}</p>
+    </div>
+  );
+};
+
+export default Card;
