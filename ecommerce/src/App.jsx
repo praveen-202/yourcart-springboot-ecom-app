e736f12efec26858b42fe74121727f17d782a21b
Praveen Kumar <kumarpraveen7604@gmail.com>
2025-10-23 18:07:23 +0530

    added a new card Card1.jsx

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Card from "./components/card";
+import Card1 from "./components/Card1";
 
 function App() {
   return (
@@ -10,6 +11,12 @@ function App() {
         studentClass="10th Grade"
         roll="23"
         school="VIT High School"
+      />
+        <Card1
+        empId="E123"
+        name="Praveen Kumar"
+        sal="50000"
+        companyName="Nebulytix Technologies Pvt. Ltd."
       />
     </div>
   );
